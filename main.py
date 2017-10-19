2e1ce78e4ac93153541f4c06f3038cbd496e196c
Alex Goodman <alexgood@stanford.edu>
2017-10-19 01:47:30 -0700

    mapped out a little more of generate_image and imported from DCGAN TF

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 #! /usr/bin/env python
 
 import numpy as np
+import tensorflow as tf
+
+from model import *
+from utils import *
 
 DIM = ???
 
 
-
-
 def generate_image(z):
 	# generate image! :)
+	network = # load our pre-existing model from checkpoints
+    img_vector = network.generator(z)
+    # use imsave() and save_images() to save into a path (from utils.py)
+    
+    
 	pass
 	## for reference: (from `DCGAN-tensorflow/utils.py`)
 	# def visualize(sess, dcgan, config, option):
